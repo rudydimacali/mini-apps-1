95b17bdf1ac09ec1a9e3759d1708a77a0968f887
Rudy <rudydimacali@gmail.com>
2018-12-10 12:12:13 -0800

    Add click listeners. Alternate between X and O.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,20 @@
+var nextPlayer = "X";
+var gameBoard = [];
 
+let toggleBox = e => {
+  if (!e.target.textContent) {
+    document.getElementById(e.target.id).textContent = nextPlayer;
+    if (nextPlayer === "X") {
+      nextPlayer = "O";
+    } else {
+      nextPlayer = "X";
+    }
+  }
+};
 
+var boxes = document.getElementsByClassName("box");
+console.log(boxes);
+for (var i = 0; i < boxes.length; i++) {
+  console.log("test");
+  boxes[i].addEventListener("click", toggleBox);
+}
