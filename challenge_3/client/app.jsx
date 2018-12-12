9e5f9431b85f8aee034afb7b6e7a4da0244c61f6
Rudy <rudydimacali@gmail.com>
2018-12-12 11:59:28 -0800

    Initialize Express server, React, and Babel.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,24 @@
+/* 
+The homepage of your application should have a Checkout button, which when clicked,
+takes the user to the first of several forms. We'll call the forms F1, F2, F3.
 
+* F1 collects name, email, and password for account creation.
+* F2 collects ship to address(line 1, line 2, city, state, zip code) and phone number.
+* F3 collects credit card #, expiry date, CVV, and billing zip code.
 
+At each step, a Next button allows the user to progress to the next step in the 
+checkout process. The final step is a confirmation page which summarizes the data
+collected in the prior three steps. This page contains a Purchase button that
+completes the purchase. When the purchase is complete, the user is returned to the homepage.
+
+*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Hello, WORLD</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("App"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,8 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const babel = require("babel-core");
 
+app.use(express.static("public"));
 
+app.listen(8080);
