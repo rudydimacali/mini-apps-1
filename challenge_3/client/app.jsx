3f68966335fa3c237c582afdba74265fdd2bd766
Rudy <rudydimacali@gmail.com>
2018-12-12 13:38:13 -0800

    Create page layouts for homepage, forms, and complete purchase.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -15,9 +15,118 @@ completes the purchase. When the purchase is complete, the user is returned to t
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      page: 0
+    };
+    this.nextPage = this.nextPage.bind(this);
+  }
+  nextPage() {
+    if (this.state.page === 4) {
+      this.setState({ page: 0 });
+    } else {
+      this.setState({ page: this.state.page + 1 });
+    }
+  }
+  // HOMEPAGE TO CHECKOUT
+  pageZeroView() {
+    return (
+      <div>
+        <h1>Page {this.state.page + 1}</h1>
+        <button
+          type="submit"
+          name="checkout"
+          id="checkout"
+          onClick={this.nextPage}
+        >
+          Checkout
+        </button>
+      </div>
+    );
+  }
+  // FORM ONE - COLLECTS NAME, EMAIL, AND PASSWORD FOR ACCOUNT CREATION
+  pageOneView() {
+    return (
+      <div>
+        <h1>Page {this.state.page + 1}</h1>
+        <button
+          type="submit"
+          name="next"
+          id="pageOneNext"
+          onClick={this.nextPage}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+  // FORM TWO - COLLECTS SHIP TO ADDRESS (LINE 1, LINE 2, CITY, STATE, ZIP) AND PHONE #
+  pageTwoView() {
+    return (
+      <div>
+        <h1>Page {this.state.page + 1}</h1>
+        <button
+          type="submit"
+          name="next"
+          id="pageTwoNext"
+          onClick={this.nextPage}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+  // FORM THREE - COLLECTS CREDIT CARD #, EXPIRATION, CVV, AND BILLING ZIP
+  pageThreeView() {
+    return (
+      <div>
+        <h1>Page {this.state.page + 1}</h1>
+        <button
+          type="submit"
+          name="next"
+          id="pageThreeNext"
+          onClick={this.nextPage}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+  // SUMMARIZE PURCHASE DATA PAGE
+  pageFourView() {
+    return (
+      <div>
+        <h1>Page {this.state.page + 1}</h1>
+        <button
+          type="submit"
+          name="purchase"
+          id="purchase"
+          onClick={this.nextPage}
+        >
+          Purchase
+        </button>
+      </div>
+    );
   }
   render() {
-    return <h1>Hello, WORLD</h1>;
+    switch (this.state.page) {
+      case 0:
+        return this.pageZeroView();
+        break;
+      case 1:
+        return this.pageOneView();
+        break;
+      case 2:
+        return this.pageTwoView();
+        break;
+      case 3:
+        return this.pageThreeView();
+        break;
+      case 4:
+        return this.pageFourView();
+        break;
+      default:
+        return this.pageZeroView();
+    }
   }
 }
 
